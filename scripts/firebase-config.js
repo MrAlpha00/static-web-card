686a81e178f650afb36faf99a8c832f8070cc605
Mr Alpha <147964466+MrAlpha00@users.noreply.github.com>
2025-11-25 23:17:19 +0530

    Add Firebase configuration file
    Added Firebase configuration with API key and project details.

---
+++ scripts/firebase-config.js
@@ -0,0 +1,10 @@
+// TODO: paste your real Firebase config here
+const firebaseConfig = {
+  apiKey: "AIzaSyDATeYRC4tcA70M5UKOo9urLti5taPSlow",
+  authDomain: "staic-app.firebaseapp.com",
+  projectId: "staic-app",
+  storageBucket: "staic-app.firebasestorage.app",
+  messagingSenderId: "73226909612",
+  appId: "1:73226909612:web:9e1256b7cc2faaa771ac09",
+  measurementId: "G-SP4B84LXXB"
+};
